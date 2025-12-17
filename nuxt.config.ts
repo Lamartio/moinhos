8862690b2063145b6bc8dd621462355f2c8d270e
Lamartio <lamarti.danny@gmail.com>
2025-12-17 20:17:22 +0000

    remove old root for domain hosting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
   },
 
   // If deploying to https://<username>.github.io/<repo-name>/
-  app: {
-    baseURL: '/moinhos/'
-  }
+  // app: {
+  //   baseURL: '/moinhos/'
+  // }
 })
