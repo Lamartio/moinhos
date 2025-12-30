8ca29913947a545e4f0e75f4f932ae8191489d52
Lamartio <lamarti.danny@gmail.com>
2025-12-30 22:48:25 +0000

    Update forms

--- app/app.vue
+++ app/app.vue
@@ -2,4 +2,29 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+
+  <!-- Hidden Netlify Form Declarations (for SSR form detection) -->
+  <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" hidden>
+    <input type="text" name="bot-field">
+    <input type="text" name="name">
+    <input type="email" name="email">
+    <input type="tel" name="phone">
+    <input type="text" name="countryCode">
+    <input type="text" name="subject">
+    <textarea name="message" />
+  </form>
+
+  <form name="booking" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" hidden>
+    <input type="text" name="bot-field">
+    <input type="text" name="name">
+    <input type="email" name="email">
+    <input type="tel" name="phone">
+    <input type="text" name="countryCode">
+    <input type="text" name="subject">
+    <input type="text" name="propertyName">
+    <input type="date" name="checkIn">
+    <input type="date" name="checkOut">
+    <input type="number" name="guests">
+    <textarea name="message" />
+  </form>
 </template>
