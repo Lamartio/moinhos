a372fe72e4b2ddbb9e7e9a3de2cfdfca9bbed631
Lamartio <lamarti.danny@gmail.com>
2025-12-31 14:23:02 +0000

    Remove sample

--- app/constants.ts
+++ app/constants.ts
@@ -1,8 +1,6 @@
 // Site-wide constants
 // Replace these placeholders with actual values
 
-export const EMAIL_PLACEHOLDER = 'contact@example.com'
-
 export const FARM_ADDRESS = `Moinhos Do Dao - Eco Quinta
 Dao Region
 Portugal`
